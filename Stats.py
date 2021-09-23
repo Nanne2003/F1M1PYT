53dcea27bce3818020440cf20f0be963ad8a15b7
Nanne <34183@ma-web.nl>
2021-09-23 12:47:09 +0200

    Stats

---
+++ Stats.py
@@ -0,0 +1,14 @@
+name = "Nanne"
+abilit = "Driving"
+lengt = "1.80"
+healt = "90%"
+leve = "100"
+
+print("SPECS NANNE KRAMER")
+print("Naam= " + name)
+print("Ability= " + abilit)
+print("Lengte= " + lengt)
+print("Health= " + healt)
+print("Level= " + leve)
+
+
