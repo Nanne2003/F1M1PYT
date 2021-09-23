4b4fe53dc055ec44d7e1559551f2dad940129054
Nanne <34183@ma-web.nl>
2021-09-23 11:22:29 +0200

    vers toevoeging

---
+++ vers.py
@@ -0,0 +1,9 @@
+
+vers1 = "Mijn naam is Nanne Kramer"
+vers2 = "Ik zit op het ma"
+vers2 = "Ik hou van aan scooters klussen en ik rij er ook graag op"
+vers3 = " Ik woon in heemstede en heb leuke vrienden"
+vers4 = "Met hun drink ik ook graag bier. Dit brengt mij veel plezier"
+
+print(vers1,vers2,vers3,vers4)
+
